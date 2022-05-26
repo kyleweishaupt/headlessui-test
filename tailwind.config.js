eaa9e89f5865fc610c5bc6d844ae3bebb50e1e32
Kyle Weishaupt <kyleweishaupt@users.noreply.github.com>
2022-05-26 13:45:14 -0400

    Dependency update.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 module.exports = {
-	mode: 'jit',
-	purge: [
-		'./index.html',
-		'./src/**/*.{vue,js,ts,jsx,tsx}'
-	],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
-}
+  mode: "jit",
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
