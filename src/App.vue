94dbde07b579f63e050c86250ef7a18e9c4d2f3b
Kyle Weishaupt <kyle@northriverdesign.com>
2022-09-14 22:54:57 -0400

    Add nav menu.

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <nav-menu />
   <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
     <img alt="Vue logo" src="@/assets/logo.png" class="mx-auto" />
     <navigation-bar />
@@ -9,11 +10,13 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import NavigationBar from "@/components/NavigationBar.vue";
+import NavMenu from "@/components/NavMenu.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     NavigationBar,
+    NavMenu,
   },
 });
 </script>
